73ca9e0b34a7f782679ac8a26558be96168bf082
LiamOrovec <lorovec@hotmail.com>
2017-04-10 15:20:10 -0300

    updated for board price in super

--- RailRoad.java
+++ RailRoad.java
@@ -3,15 +3,16 @@ public class RailRoad extends Property{
    private static int[] rents = {25, 50, 100, 200};
    private int rent;
    
-   public RailRoad(int p, String n){
-      super(p, n);
+   public RailRoad(int p, int pr, String n){
+      super(p, pr, n);
       rent = 25;
    }
    
    public int getRent(){
+      this.setRent();//updates rent if needed
       return rent;
    }
    public void setRent(){
-      rent = rents[this.getOwner().getRails() -1];//Picks rent acording to number of railroads owned
+      rent = rents[this.getOwner().getRails() -1];//Picks rent acording to number of railroads owned, to be run when getting rent
    }
 }
